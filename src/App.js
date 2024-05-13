96be812022897aec6e41bc04df73c6b2e114192f
Giovanna-Sant <146599767+Giovanna-Sant@users.noreply.github.com>
2024-05-13 11:07:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
