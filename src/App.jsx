6f6787acd499e44f606df34cce531c7c211b2889
gabriel <gabrieldosanjosdbz@gmil.com>
2024-05-13 11:55:22 -0300

    fix: all update for vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import "./footer.css"   
+import logoFooter from "../../img/logoFooter.svg"
+import github from "../../img/github.svg"
+import linktree from "../../img/linktree.svg"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="right">
+                <span id="line-roxa"></span>
+                <span id="line-verde"></span>
+            </div>
+
+            <div className="flex">
+                <img src={logoFooter} alt="logo footer" id="logo-footer"/>
+
+                <div>
+                    <div className="txt">
+                        <p>A GymRats foi um projeto pensado e desenvolvido para fins educacionais pela equipe da turma <span className="verde">2DM</span> do <span className="verde">SENAI Suíço-Brasileiro</span></p>
+                        <p>Acompanhe o desenvolvimento de nosso projeto e <span className="roxo">siga nossos devs!</span></p>
+                    </div>
+
+                    <img src={github}   className="icon" alt="icon github"/>
+                    <img src={linktree} className="icon" alt="icon linktree"/>
+                </div>
+            </div>
+            <div className="right">
+                <p>© GymRats 2024, SENAI Suíço</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
