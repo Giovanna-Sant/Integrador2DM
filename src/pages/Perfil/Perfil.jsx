0b0f948ff17afc4cc44945dbe79cca1b727950fe
samuelleitee <programmer.and.investor@gmail.com>
2024-06-14 10:49:43 -0300

    update botao pagina perfil

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,20 +1,77 @@
 import "./perfil.css"
-import {Link} from "react-router-dom"
+import { useState, useEffect } from "react"
+import api from '../../config/axios'
+import perfil from "../../img/Icons/perfil.svg"
+import deleteicon from "../../img/Icons/TrashCan.png"
+import {Link, useNavigate} from "react-router-dom"
+import editicon from "../../img/Icons/Pencil.svg"
+import logoutIcon from "../../img/Icons/logout.png"
+import { jwtDecode } from "jwt-decode"
+
 
 const Perfil = () => {
+
+  const navigate = useNavigate()
+
+  const [nome, setNome] = useState("")
+  const [sobrenome, setSobrenome] = useState("")
+  const [email, setEmail] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [editar, setEditar] = useState(false)
+
+  const permitirAtualizar = () => {
+    setEditar(!editar)
+    let botao = document.querySelector('.linkEditarPessoais')
+
+    let inp = [...document.querySelectorAll('.inativo')]
+
+
+    inp.map( e => {
+      e.classList.toggle('ativo')
+    })
+  }
+
+  const deletePerfil = async () => {
+    if(confirm("Deseja realmente deletar sua conta?")){
+      axios.delete('')
+        .then(function(response){
+          alert('Conta deletada com sucesso')
+          window.location.href = '/'
+        })
+        .catch(function(error){
+          alert('Erro ao deletar conta')
+        })
+    }
+  }
+
+  // function mudarCLasse() {
+  //   let botao = document.querySelector('.linkEditarPessoais')
+  //   let inp = document.querySelector('.inativo')
+
+  //   inp.classList.toggle('ativo')
+    
+  // }
+  
+  const [user, setUser] = useState({})
+
   return (
     <div className="Perfil">
       <div className="divPerfil">
         <div className="perfilFuncoes">
           <Link className="linkPerfil" to="/">
-            <img className="perfilIcon" src="src/img/Icons/perfil.svg"/>
+            <img className="perfilIcon" src={perfil}/>
             <p className="fonte-subtitulos">Conta</p>
-         </Link>
-        <hr/>
-        <Link className="linkDelete" to="/">
-            <img className="deleteIcon" src="src/img/Icons/TrashCan.png"/>
-            <p className="fonte-subtitulos">Deletar Conta</p>
-        </Link>
+          </Link>
+          <hr />
+          <button onClick={deletePerfil} className="linkDelete">
+              <img className="deleteIcon" src={deleteicon}/>
+              <p className="fonte-subtitulos">Deletar Conta</p>
+          </button>
+          <hr />
+          <button className="linkLogout">  
+              <img className="logoutIcon" src={logoutIcon}/>
+              <p className="fonte-subtitulos">Logout</p>
+          </button>
         </div>
         <div className="infoDiv">
 
@@ -22,34 +79,34 @@ const Perfil = () => {
             <div className="topinfo">
               <p className="fonte-05">Informações Pessoais</p>
 
-              <Link className="linkEditarPessoais">  
-                <img className="editIcon" src="src/img/Icons/Pencil.svg"/>
+              <button onClick={permitirAtualizar} className="linkEditarPessoais">
+                <img className="editIcon" src={editicon}/>
                 <p className="fonte-subtitulos">Editar</p>
-              </Link>
+              </button>
             </div>
             
             <div className="pessoalinfo">
               <div>
                 <p className="fonte-subtitulos">Nome</p>
-                <p className="fonte-gerais">Fulano</p>
+                <input value={nome} onChange={(e) => setNome(e.target.value)} className="inativo" readOnly={!editar}>{user.nome}</input>
               </div>
 
-            <div className="sobrenomediv">
+              <div className="sobrenomediv">
                 <p className="fonte-subtitulos">Sobrenome</p>
-                <p className="fonte-gerais">de Tal</p>
+                <input value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} className="inativo" readOnly={!editar}>{user.sobrenome}</input>
               </div>
             </div>
 
             <div className="pessoalinfo2">
-            <div>
+              <div>
                 <p className="fonte-subtitulos">Email</p>
-                <p className="fonte-gerais">fulano@email.com</p>
-            </div>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} className="inativo" readOnly={!editar}>{user.email}</input>
+              </div>
 
-            <div className="phonediv">
+              <div className="phonediv">
                 <p className="fonte-subtitulos">Telefone</p>
-                <p className="fonte-gerais">(11)4002-8922</p>
-            </div>
+                <input value={telefone} onChange={(e) => setTelefone(e.target.value)} className="inativo" readOnly={!editar}>{user.telefone}</input>
+              </div>
             </div>
           </div>
 
@@ -57,10 +114,10 @@ const Perfil = () => {
             <div className="topinfo">
               <p className="fonte-06">Seu Plano</p>
 
-              <Link className="linkEditarPessoais">  
-                <img className="editIcon" src="src//img/Icons/Pencil.svg"/>
+              <button className="linkEditarPessoais">  
+                <img className="editIcon" src={editicon}/>
                 <p className="fonte-subtitulos">Editar</p>
-              </Link>
+              </button>
             </div>
 
             <div className="planoinfo">
