7d344e7bd76795e2eb713254a0287446db924138
liviaBELLO <livia_bello@outlook.com>
2024-06-14 11:34:32 -0300

    merge completo

--- src/pages/Planos/index.jsx
+++ src/pages/Planos/index.jsx
@@ -1,5 +1,5 @@
 import './style.css'
-import '../../App.css'
+import '../../App.cs'
 import ContainerTitle from './components/ContainerTitle';
 import PlanosDisponiveis from './components/PlanosDisponiveis';
 
