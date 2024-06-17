0da725c27036cfa49e42449b8742a47f4833dc17
Giovanna Santos <146599767+Giovanna-Sant@users.noreply.github.com>
2024-06-17 00:12:24 -0300

    Import imagens Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,6 +7,9 @@ import { jwtDecode } from "jwt-decode"
 import Art from '../../components/Art/Art';
 import Header from '../../components/Header/Header'
 
+import logo from "src/img/Logo.svg"
+import emoji from "src/img/Icons/emoji.svg"
+
 const Login = () => {
     const navigate = useNavigate();
     const user = localStorage.getItem('authenticated');
@@ -43,8 +46,8 @@ const Login = () => {
                 </div>
                 <div className="login-right-box">
                     <div className="login-right-box-content">
-                        <img src="src\img\Logo.svg" alt="Logo da GymRats" id="login-flogo"/>
-                        <h2 id="login-ftitle">Bem Vindo (a) De <span id="bracao">Volta <img src="src\img\Icons\emoji.svg" alt="Braço musculoso palmeiras"/></span></h2>
+                        <img src={logo} alt="Logo da GymRats" id="login-flogo"/>
+                        <h2 id="login-ftitle">Bem Vindo (a) De <span id="bracao">Volta <img src={emoji} alt="Braço musculoso palmeiras"/></span></h2>
                         <p id="login-solcred">Por favor, digite suas credenciais para acessar sua conta</p>
                         <form method="post" id="cadastro" onSubmit={(e) => handleSubmit(e)}>
                             <div className="login-ent">
