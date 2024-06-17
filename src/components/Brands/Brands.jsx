1147272bbc2dc0b2c0e3cc283d8e890019203885
Giovanna Santos <146599767+Giovanna-Sant@users.noreply.github.com>
2024-06-16 23:56:33 -0300

    Import imagens Brands.jsx

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,21 +1,26 @@
 import './brands.css'
 
+import growth from "src/img/Marcas/growth.png"
+import nike from "src/img/Marcas/nike.png"
+import smartfit from "src/img/Marcas/smartfit.png"
+import insider from "src/img/Marcas/insider.png"
+
 const Brands = () => {
     return (
         <div className="brand-cont">
         <p className="cor-0 fonte-subtitulos">Temos uma relação de confiança com nossos parceiros!</p>
             <div className='marcas'>
                 <a href="https://www.gsuplementos.com.br" target="_blank">
-                <img src="src\img\Marcas\growth.png"   alt="logo Growth" />
+                <img src={growth}   alt="logo Growth" />
                 </a>
                 <a href="https://www.nike.com/"        target="_blank">
-                <img src="src\img\Marcas\nike.png"     alt="logo Nike" />
+                <img src={nike}     alt="logo Nike" />
                 </a>
                 <a href="https://www.smartfit.com.br/" target="_blank">
-                <img src="src\img\Marcas\smartfit.png" alt="logo SmartFit" />
+                <img src={smartfit} alt="logo SmartFit" />
                 </a>
-                <a href="https://insiderstore.com.br/" target="_blank">
-                <img src="src\img\Marcas\insider.png"  alt="logo Insider" />
+                <a href={insider} target="_blank">
+                <img src=  alt="logo Insider" />
                 </a>
             </div>
         </div>
